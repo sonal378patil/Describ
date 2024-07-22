e01e307d9a91c7b1f881cbf3fc75b19b6b5b697b
ankita.patil <ankita.patil@LAP-ANKITAP.sumasoft.in>
2024-07-22 22:29:01 +0530

    hivari

---
+++ src/Demorung/Funrun.java
@@ -0,0 +1,30 @@
+package Demorung;
+
+public class Funrun {
+
+	public static void main(String[] args) {
+		String sname; //datatype varibale name
+		int srollnumb;
+		float sper;
+		char Greade;
+		
+		
+		//2variable initilization 
+	     
+	     sname="amol"; //variablename="variable value"
+	     srollnumb=101;
+	     sper=85.23f;
+	     Greade='A';
+	     
+		
+	     //3 variable usage (print variable info)
+	     System.out.println("student name   :"   +sname);
+	     System.out.println( "student number  : "+srollnumb);
+	     System.out.println( "student persentage : "+sper+"%");
+	     System.out.println( "student grade   :"+Greade);
+	     //https://github.com/ankita83patil/Justdemo.git
+	 }
+
+}
+
+	
